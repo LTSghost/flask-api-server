7075a250682c1e180718ada488ca440c54615069
LTS <DKLTSghost@gmail.com>
2023-07-20 13:48:06 +0800

    delete: del redundant

--- app/routes/sys_menu.py
+++
@@ -1,9 +0,0 @@
-from .sys_user import sys_api
-from flask_restx import Resource
-
-sys_menu = sys_api.namespace('sys_menu', description='sys operations')
-
-@sys_menu.route('/test')
-class getTest(Resource):
-    def get(self):
-        return {'message': 'test'}
